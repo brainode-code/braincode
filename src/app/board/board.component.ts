ed19a6d093f4fef102b3919515071f8eb6ed337a
adiyy2001 <39469489+adiyy2001@users.noreply.github.com>
2020-07-08 23:48:10 +0200

    Update board.component.ts

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BoardComponent {
 }
