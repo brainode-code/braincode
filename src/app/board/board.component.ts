0883e0ae9ac551c8d9339615dbfd2ed12bf10be5
Maciej Rapacki <mac.rapacki@gmail.com>
2020-07-10 15:34:15 +0100

    Change from CSS to SCSS minor changes

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.scss']
 })
 export class BoardComponent { }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
 
