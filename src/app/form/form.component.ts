369a42913491c76e47cb20f8d3187bc63f486cb3
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2020-07-12 18:19:06 +0200

    add getting value of inputs and create empty array

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,4 +12,17 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  valuesArras = new Array;
+  title:string = "";
+  description:string ="";
+  deadline:Date;
+  value:number;
+  
+  getInputsValue(event: any): void { 
+    event.preventDefault()
+    console.log(this.title);
+    console.log(this.description);
+    console.log(this.deadline);
+    console.log(this.value);
+  }
 }
