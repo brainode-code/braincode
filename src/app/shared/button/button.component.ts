266c1504dfcecf9511fbbc5e5461771a8045d664
Klaudia Michalczyszyn <k.michalczyszyn@gmail.com>
2020-07-15 20:49:50 +0200

    Fixed form

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent { }
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
